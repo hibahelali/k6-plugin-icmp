47917a5c7222fe5e34ee94cc5c5315216ca14b01
hibahelali <95078325+hibahelali@users.noreply.github.com>
2024-11-29 12:25:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # This fetches the k6-plugin-icmp from GitHub and
-# builds it in your $GOPATH/src/github.com/mostafa/k6-plugin-icmp
+# builds it in your $GOPATH/src/github.com/hibahelali/k6-plugin-icmp
 
-go build -buildmode=plugin -ldflags="-s -w" -o icmp.so github.com/mostafa/k6-plugin-icmp
+# Assurez-vous d'être dans le bon répertoire
+cd $GOPATH/src/github.com/hibahelali/k6-plugin-icmp || exit
+
+# Construire le plugin avec le nouveau chemin d'importation
+go build -buildmode=plugin -ldflags="-s -w" -o icmp.so go.k6.io/k6/plugin
+
